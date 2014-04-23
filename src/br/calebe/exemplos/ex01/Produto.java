a6d0f735abe082ae08f30bd7596c1e851ddd411e
Vinicius <Vinicius@Vinicius-PC>
2014-04-22 23:44:55 -0300

    Adicionadas novas funcionalidades:
    adicionar outros tipos de produtos ao carrinho
    listar todos os produtos do carrinho
    remover um produto do carrinho
    calcular o total que deve ser pago pelos produtos do carrinho

--- src/br/calebe/exemplos/ex01/Produto.java
+++ src/br/calebe/exemplos/ex01/Produto.java
@@ -2,26 +2,34 @@ package br.calebe.exemplos.ex01;
 
 public class Produto {
 
-    private String nome;
-    private double preco;
-    private String tipo;
+	private String nome;
+        private String tipo;
+	private double preco;
+        
 
-    public Produto(String nome, double preco, String tipo) {
-        this.nome = nome;
-        this.preco = preco;
-        this.tipo = tipo;
-    }
+	public Produto(String nome, double preco, String tipo) {
+		this.nome = nome;
+		this.preco = preco;
+                this.tipo = tipo;
+	}
 
-    public double getPreco() {
-        return preco;
-    }
+	public double getPreco() {
+		return preco;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        return equals((Produto) obj);
-    }
+	@Override
+	public boolean equals(Object obj) {
+		Produto p = (Produto) obj;
+		return nome.equals(p.nome);
+	}
+        
+	public String getName() {
+		return this.nome;
+	}
+     
+	public String getTipo() {
+		return this.tipo;
+	}
+        
 
-    public boolean equals(Produto obj) {
-        return nome.equals(obj.nome);
-    }
 }
