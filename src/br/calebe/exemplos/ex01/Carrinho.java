ab19967f34426e10bdd22b7dc1974f7bce8f1dc8
Vinicius <Vinicius@Vinicius-PC>
2014-04-22 23:44:48 -0300

    Adicionadas novas funcionalidades:
    adicionar outros tipos de produtos ao carrinho
    listar todos os produtos do carrinho
    remover um produto do carrinho
    calcular o total que deve ser pago pelos produtos do carrinho

--- src/br/calebe/exemplos/ex01/Carrinho.java
+++ src/br/calebe/exemplos/ex01/Carrinho.java
@@ -5,26 +5,63 @@ import java.util.List;
 
 public class Carrinho {
 
-    private List<Produto> produtos;
+	private List<Produto> produtos;
 
-    public Carrinho() {
-        produtos = new ArrayList<>();
-    }
+	public Carrinho() {
+		produtos = new ArrayList();
+	}
 
-    public void add(Produto produto) {
-        produtos.add(produto);
-    }
+	public void add(Produto produto) {
+		produtos.add(produto);
+	}
+        
+        public int getQuantity() {
+		return this.produtos.size();
+	}
 
-    public Produto menorProduto() throws CarrinhoVazioExpected {
-        if (produtos.isEmpty()) {
-            throw new CarrinhoVazioExpected();
-        }
-        Produto menor = produtos.get(0);
-        for (Produto produto : produtos) {
-            if (produto.getPreco() < menor.getPreco()) {
-                menor = produto;
+	public Produto menorProduto() throws CarrinhoVazioExpected {
+		if (produtos.isEmpty())
+			throw new CarrinhoVazioExpected();
+		Produto menor = produtos.get(0);
+		for (Produto produto : produtos) {
+			if (produto.getPreco() < menor.getPreco())
+				menor = produto;
+		}
+		return menor;
+	}
+        
+        public void showAll () throws CarrinhoVazioExpected {
+                if (produtos.isEmpty())
+                    throw new CarrinhoVazioExpected();
+		for (Produto p : this.produtos) {			
+			System.out.println("Produto:" + p.getName() + " Tipo:" + p.getTipo() + " Valor:" + p.getPreco());
+		}
+		System.out.println();
+	}
+        
+        public boolean RemoveProduto(String nomeProduto) throws CarrinhoVazioExpected{
+            if (produtos.isEmpty())
+                throw new CarrinhoVazioExpected();
+            for(Produto p : this.produtos) {
+                    if (p.getName().equals(nomeProduto)) {
+                            this.produtos.remove(p);
+                            return true;
+                    }
             }
-        }
-        return menor;
-    }
+            return false;
+	}
+        
+        public double calculaValorTotal() throws CarrinhoVazioExpected{
+                double valor;
+                valor = 0;
+                if (produtos.isEmpty())
+                throw new CarrinhoVazioExpected();
+		for (Produto p : this.produtos) {			
+                    valor += p.getPreco();
+                    
+		}
+		return valor;
+	}
+
+
 }
