b4d7f658a394076542b939130e13979a4df1950d
Vinicius <Vinicius@Vinicius-PC>
2014-04-29 00:16:07 -0300

    Criados métodos: pagamentoViaCartao CreditocontroleEstoque

--- src/br/calebe/exemplos/ex02/ClasseExemplo.java
+++ src/br/calebe/exemplos/ex02/ClasseExemplo.java
@@ -18,6 +18,34 @@ public class ClasseExemplo {
     public void run(int i) {
         answer = controller.metodo(i);
     }
+
+    public void pagueNoCartaoCredito(double valor){
+        
+        try{
+            Thread.sleep(700);
+            System.out.println("aguardando pagamento");
+            Thread.sleep(300);
+            answer = controller.pagamentoViaCartaoCredito(valor);
+            System.out.println("pago");
+        }catch(Exception e){
+            System.out.println("Houve alguma falha no pagamento");
+        }
+        
+    }
+    
+    public void controleEstoque(int itens){
+        try{
+            answer = controller.controleEstoque(itens);
+            Thread.sleep(400);
+            System.out.println("empacotando");
+            Thread.sleep(500);
+            System.out.println("enviado para entrega");
+            Thread.sleep(200);
+            System.out.println("Entregue!!!!!");
+        }catch(Exception e){
+            System.out.println("Falta de produtos no estoque!!!!!");
+        }
+    }
     
     public String getAnswer() {
         return answer;
